810996dcdf76be1fb800e1f8a15b04f97375107c
Ganar KF <gnrkfz@gmail.com>
2024-08-10 21:43:07 +0700

    Update loadSession.py

--- loadSession.py
+++ loadSession.py
@@ -1,9 +1,9 @@
 from telethon.sync import TelegramClient, events
 import re
 
-api_id = '21346711'
-api_hash = 'fd9a5c2d2d8cde393adaab0dfcc9d300'
-session = '6285156393610.session'
+api_id = '###'
+api_hash = '######'
+session = '###.session'
 
 client = TelegramClient(session, api_id, api_hash)
 
