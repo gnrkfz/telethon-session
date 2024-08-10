9a24ca312bca095be62539897e8c194193fdb9fb
Ganar KF <gnrkfz@gmail.com>
2024-08-10 21:42:41 +0700

    Update saveSession.py

--- saveSession.py
+++ saveSession.py
@@ -1,7 +1,7 @@
 from telethon.sync import TelegramClient
 
-api_id = '21346711'
-api_hash = 'fd9a5c2d2d8cde393adaab0dfcc9d300'
+api_id = '###'
+api_hash = '######'
 
 with TelegramClient('sesi1.session', api_id, api_hash) as client:
-    print("Sukses")
+    print("Session Berhasil")
