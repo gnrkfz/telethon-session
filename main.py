bc07b2de7e37f7b6a4521e2368d7e9fbc793739c
Ganar KF <gnrkfz@gmail.com>
2024-12-12 16:01:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from telethon.sync import TelegramClient, events
 import re
 
-api_id = '###'
-api_hash = '######'
-session = '###.session'
+api_id = '#############'
+api_hash = '###########'
+session = 'tele.session'
 
 client = TelegramClient(session, api_id, api_hash)
 
+# Listening Incoming Messages
 @client.on(events.NewMessage)
 async def handle_incoming_message(event):
     print(event.text)
-    # otp = re.search(r'\b(\d{5})\b', event.raw_text)
-    # if otp:
-    #     print("OTP : ", otp.group(0))
+    #
+    #
+    #
 
 print("Listening for messages...")
 
